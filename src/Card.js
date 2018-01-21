285603b3920fac5d8fff89ae791b497803f48abe
Ariane Hartmann <ariane.ermelind.hartmann@gmail.com>
2018-01-21 10:32:00 +0100

    Installed bootstrap for usage of adding styling to the app and created first card component

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Card.css';
+
+export class Card extends Component {
+
+  render() {
+    return (
+      <div className="card-box">
+
+      </div>
+    );
+  }
+}
+
+export default Card;
