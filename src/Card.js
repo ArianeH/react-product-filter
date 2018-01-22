068ba0e2567aa3391a091c4f495c346d7691b7c9
Ariane Hartmann <ariane.ermelind.hartmann@gmail.com>
2018-01-22 10:01:23 +0100

    Added overlay element and cta buttons to card and added styling to these elements

--- src/Card.js
+++ src/Card.js
@@ -8,6 +8,9 @@ export class Card extends Component {
       <a href="#" className="product-card">
         <div className="product-card-content">
           <img src={this.props.image} alt="Display of fabric" className="product-img"/>
+          <div className="product-card-overlay"></div>
+          <div id="view-details">Click for more details</div>
+          <div id="add-now-btn">Add to cart</div>
           <div className="product-details-container">
             <h2 className="product-card-heading">{this.props.name}</h2>
             <p className="description">{this.props.description}</p>
